19b2bc16096909ba8cbe4935bd0a08b912c18ece
Arthur Julião <azrael.zila@gmail.com>
2019-01-09 22:02:30 -0200

    Kills devem acrescer contagem de total

--- parser.py
+++ parser.py
@@ -9,19 +9,21 @@ def ignore(details, kills, game, games, players_dict):
 
 def kill(details, kills, game, games, players_dict):
     '''
-    # Morte por <world> subtraí pontuação da vítima
-    >>> kill(['1022', '7', '22:', '<world>', 'killed', 'Mal', 'by', 'MOD_TRIGGER_HURT'], {}, {}, [], {})
-    ({'7': -1}, {}, {})
+        # Morte por <world> subtraí pontuação da vítima
+        >>> kill(['1022', '7', '22:', '<world>', 'killed', 'Mal', 'by', 'MOD_TRIGGER_HURT'], {}, {}, [], {})
+        ({'7': -1}, {'total_kills': 1}, {})
 
-    # Morte por jogador adiciona pontuação ao jogador
-    >>> kill(['22', '7', '22:', '<world>', 'killed', 'Mal', 'by', 'MOD_TRIGGER_HURT'], {}, {}, [], {})
-    ({'22': 1}, {}, {})
+        # Morte por jogador adiciona pontuação ao jogador
+        >>> kill(['22', '7', '22:', '<world>', 'killed', 'Mal', 'by', 'MOD_TRIGGER_HURT'], {}, {}, [], {})
+        ({'22': 1}, {'total_kills': 1}, {})
 
-    # Suicídio não afeta pontuação
-    >>> kill(['7', '7', '22:', '<world>', 'killed', 'Mal', 'by', 'MOD_TRIGGER_HURT'], {}, {}, [], {})
-    ({}, {}, {})
+        # Suicídio não afeta pontuação
+        >>> kill(['7', '7', '22:', '<world>', 'killed', 'Mal', 'by', 'MOD_TRIGGER_HURT'], {}, {}, [], {})
+        ({}, {'total_kills': 1}, {})
     '''
     killer, victim, *_ = details
+
+    game['total_kills'] = game.get('total_kills', 0) + 1
 
     if killer == '1022':
         kills[victim] = kills.get(victim, 0) - 1
@@ -30,19 +32,23 @@ def kill(details, kills, game, games, players_dict):
 
     return kills, game, players_dict
 
+def end_game(details, kills, game, games, players_dict):
+    return {}, {}, {}
+
 if __name__ ==  '__main__':
     import doctest
     doctest.testmod()
     
     f_input = open('games.log', 'r')
 
-    commands = {}
+    commands = {'Kill:':kill}
     games = []
     game = {}
     kills = {}
     players_dict = {}
 
     for line in f_input:
+        print(game, kills)
         minute, action, details = break_line(line)
         kills, game, players_dict = commands.get(action, ignore)\
                                         (details, kills, game, games, players_dict)
